e9973e998cc6859b9c811073af1ac35cf043bacb
tunime <tunime.1@gmail.com>
2018-04-03 22:28:12 -0500

    incluyendo nuevas vistas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/asistencia', function () {
+    return view('asistencia');
+});
+Route::get('/loginmain', function () {
+    return view('loginmain');
+});
+
